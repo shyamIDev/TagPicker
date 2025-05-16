13a9547418ce97bc09f24f29d327f83a850ed468
Infoenum Testing <infoenum.testing@gmail.com>
2025-05-16 14:57:44 +0530

    Initial Commit

---
+++ TagPicker/TagPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagPickerApp.swift
+//  TagPicker
+//
+//  Created by ie16 on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TagPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
